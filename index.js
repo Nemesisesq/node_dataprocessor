9d4849d81406407ba4485dbdcfb30590488ef365
Carl Lewis <Nem@Carls-MacBook-Pro.local>
2016-06-30 23:01:04 -0400

    ss

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// var ss = require('./sorter.js');
+var ss = require('./sorter.js');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -17,34 +17,34 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Add headers
-// app.use(function (req, res, next) {
+app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    // res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
-    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
 
     // Request headers you wish to allow
-    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
-    // res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
     // Pass to next layer of middleware
-    // next();
-// });
+    next();
+});
 
 app.get('/', function (request, response) {
     response.render('pages/index');
 });
 
-// app.post('/service_list', function (request, response) {
-//     var body = request.body;
-//     var res = ss.servicePanelList(body);
-//     response.send(res)
-// });
+app.post('/service_list', function (request, response) {
+    var body = request.body;
+    var res = ss.servicePanelList(body);
+    response.send(res)
+});
 
 // app.post('/checkout_list', function(request, response){
 //     var body = request.body;
