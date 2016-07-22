8ca0c8c9e07e7f46c81296c2660a8ecc5ae89250
Carl Lewis <Nem@Carls-MacBook-Pro.local>
2016-07-21 23:05:24 -0400

    sling playstation turned off

--- js/detailSorter.js
+++ js/detailSorter.js
@@ -29,7 +29,7 @@ module.exports = {
         var res
         async.waterfall([
             async.apply(this.detailSources, cs),
-            this.sonyVueProcessor
+            // this.sonyVueProcessor
         ], function (err, result) {
             res = result
         })
