5cec85471bd844a3a7bfdf43e0aa2ae06514ffed
Nem <nemesisesq@gmail.com>
2016-09-21 15:32:11 -0400

    change

--- js/servicesSorting/detailSorter.js
+++ js/servicesSorting/detailSorter.js
@@ -174,16 +174,6 @@ module.exports = {
                 .map(function (elem) {
                     if (elem) {
 
-                        // console.log(elem)
-                        // if (elem.channel != undefined) {
-                        //     console.log("I shouldn't be here")
-                        //
-                        //     _.forEach(elem.channel, function (c) {
-                        //
-                        //         c = utils.fixGuideboxData(c, elem);
-                        //     });
-                        // }
-
                         if (elem.guidebox_data != undefined) {
                             elem.source = elem.guidebox_data.short_name
                         }
